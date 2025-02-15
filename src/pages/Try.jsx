ebc63d7cc40b21929676ee4bc81980d9be3571bd
NANELJONES <eruditejones@gmail.com>
2025-02-15 17:52:10 +0000

    ba

--- src/pages/Try.jsx
+++ src/pages/Try.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { server } from '../server'
+import axios from 'axios'
 
 const Try = () => {
+  
+const sendRequest = async () => {
+axios.post(`${server}/user/send_grid`, {
+    email: "eruditejones@gmail.com",
+     message: "Hello, this is a test email"
+  }).then((res) => {
+    alert(res.data.message)
+    console.log(res)
+  }).catch((err) => {
+    console.log(err)
+  })
+  
+}
+
   return (
-    <div  className='text-blue text-[500px]'>
+   <>
+    <div  className='text-blue text-[100px]'>
       I am just seeing if it works 
     </div>
+    <button className='bg-primary_color text-white p-2 rounded-md' onClick={()=> {sendRequest()}}> Send Email </button>
+   </>
   )
 }
 
